a5ed648a3eed71c778f4a9de9c3aebedddc52228
johanC123 <johan.bolenos6323@stu.mtec.edu>
2023-10-02 23:01:18 -0600

    tarea

---
+++ Week5/app.js
@@ -0,0 +1,16 @@
+let colur = $('.navItem').css('color', 'white');
+
+let style = $('#nav').css('background-color', '#145');
+
+$("li").each(function(){
+    $(this).css("background-color", "#ccc");
+  });
+
+
+  $(".odd").each(function(){
+    $(this).css("background-color", "#aaa");
+  });
+
+let name = $('#myInput').attr('placeholder', 'Johan');
+
+
